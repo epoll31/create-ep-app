e45d3d5d62da3319d511a96db9bb3027695fa7e8
ethan <epollack31@gmail.com>
2024-06-03 15:03:45 -0700

    added optional name as argument

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,24 @@
 
 import { execSync } from "child_process";
 
-execSync(
-  `npx create-next-app --example https://github.com/epoll31/create-esp-app/tree/main/example`,
-  { stdio: "inherit" }
-);
+// Get the project name from the command line arguments
+const projectName = process.argv[2];
+
+// Check if the project name starts with a hyphen
+if (projectName && projectName.startsWith("-")) {
+  console.error("Project name cannot start with a hyphen.");
+  process.exit(1);
+}
+
+// Construct the command with or without the project name
+const command = projectName
+  ? `npx create-next-app ${projectName} --example https://github.com/epoll31/create-esp-app/tree/main/example`
+  : `npx create-next-app --example https://github.com/epoll31/create-esp-app/tree/main/example`;
+
+try {
+  // Execute the command
+  execSync(command, { stdio: "inherit" });
+} catch (error) {
+  console.error("Failed to create the project:", error.message);
+  process.exit(1);
+}
